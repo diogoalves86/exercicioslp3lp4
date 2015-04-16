537f06edfbc02eed90268282ab22dddbcccd79d6
Diogo Alves <diogoalvessilva86@gmail.com>
2015-04-16 16:56:04 -0300

    Criando classes de "Categoria" e "Produto"

--- WingtipToys/WingtipToys/Models/Category.cs
+++ WingtipToys/WingtipToys/Models/Category.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace WingtipToys.Models
 {
     public class Category
     {
-        public int MyProperty { get; set; }
+        [ScaffoldColumn(false]
+        public int CategoryID { get; set; }
+
+        [Required, StringLength(100), Display(Name= "Name")]
+        public string CategoryName { get; set; }
+
+        [Display(Name = "Product Description")]
+        public string Description { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
